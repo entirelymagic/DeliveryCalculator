01e08c495fad21d245c18446dd0bb8eed63729ac
entirelymagic@gmail.com <@lexandr@123>
2021-05-17 16:09:37 +0300

    Added DeliveryModel fields

--- calculator/models.py
+++ calculator/models.py
@@ -3,4 +3,17 @@ from django.db import models
 
 # Create your models here.
 class DeliveryModel(models.Model):
-    pass
+    hours = models.FloatField(default=0)
+    completed_deliveries = models.IntegerField(default=0)
+    delivery_price = models.FloatField(default=0)
+    bonus = models.FloatField(default=0)
+    collected_money = models.FloatField(default=0)
+    tips_on_card = models.FloatField(default=0)
+    tips_cash = models.FloatField(default=0)
+    company_fixed_commission = models.FloatField(default=0)
+    company_percentage_commission = models.FloatField(default=0)
+    driven_distance = models.FloatField(default=0)
+    fuel_consumption_100km = models.FloatField(default=0)
+    fuel_type = models.FloatField(default=0)
+    fuel_price = models.FloatField(default=0)
+
